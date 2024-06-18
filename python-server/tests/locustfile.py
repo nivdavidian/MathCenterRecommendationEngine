0277fc4d00c18c65d1a8f89445d9203c5bc61b05
Niv Davidian <nivdavidian@gmail.com>
2024-06-18 21:05:31 +0300

    adding locust testing

---
+++ python-server/tests/locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+class RecommendationUser(HttpUser):
+    wait_time = between(1, 5)  # Simulates a user waiting between 1 to 5 seconds between tasks
+
+    @task
+    def get_recommendation(self):
+        self.client.post("http://127.0.0.1:8000/api/markov", json={"uid": "16ca9f55", "cCode": "AR", "lCode": "es"})
+        # self.client.post("http://127.0.0.1:8000/api/mostpopular", json={"filters": {'MonthFilter':{'months':[1]}}, "cCode": "AR", "lCode": "es"})
